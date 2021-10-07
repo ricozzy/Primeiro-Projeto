f2b918cab97d297f28893f7b511947beedc1b8e7
Ricardo <ricozzy@hotmail.com>
2021-10-06 23:26:50 -0300

    Alteração para compra de cartas na mesma função, mas ainda trabalhando no código para melhorá-lo

--- baralho/baralho.js
+++ baralho/baralho.js
@@ -1,36 +1,39 @@
 //Jogo de Cartas 21
 
 var baralho = [11,2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10];
-var totalDeCartas = 52;
 var maoDoJogador = [];
 var maoDaBanca = [];
 var num;
+var destino;
 
-function sorteiaCarta (num) {
+function sorteiaCarta (num, destino) {
     
     for (var i = 0; i < num; i++) {
     
-    var carta = Math.floor(Math.random() * totalDeCartas);
-    totalDeCartas --;
-    maoDoJogador.push(baralho[carta]);
-    baralho.splice(carta, 1);
-
+    var carta = Math.floor(Math.random() * baralho.length);
+    
+    if (destino == 1) {
+            maoDoJogador.push(baralho[carta]);
+            baralho.splice(carta, 1);
+            
+    }
+    else { 
+            maoDaBanca.push(baralho[carta]);
+            baralho.splice(carta, 1);
+            
+    }
+      
     }
 
-    return (maoDoJogador);
-
-}
-
-function sorteiaCartaBanca () {
+    switch (destino) {
+        case 1: return (maoDoJogador);
     
-    var carta = Math.floor(Math.random() * totalDeCartas);
-    totalDeCartas --;
-    maoDaBanca.push(baralho[carta]);
-    baralho.splice(carta, 1);
-
+        case 2: return (maoDaBanca);
+    }
 }
+
 
 console.log("Seja Bem vindo ao jogo 21\n");
-console.log("As cartas iniciais do Jogador são = " + sorteiaCarta(30));
-console.log("Total de cartas do Baralho agora é de " + totalDeCartas);
+console.log("As cartas iniciais do Jogador são = " + sorteiaCarta(30, 1));
+console.log("As cartas iniciais da Banca são = " + sorteiaCarta(10, 2));
 console.log("O baralho atual é: " + baralho);
